5a379acad16234b3eca4561408ade1a0fb32e3f8
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-01 20:33:55 +0200

    done

--- Class.cs
+++ Class.cs
@@ -1,1 +1,38 @@
-﻿
+﻿namespace Domain;
+
+internal class Human
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+}
+
+internal class Builder : Human
+{
+    public string Specialization { get; set; }
+
+    public void Build()
+    {
+        Console.WriteLine($"{Name} is a {Age}-year-old {Gender} {Specialization}, who can build some cool things.");
+    }
+}
+
+internal class Sailor : Human
+{
+    public string Ship { get; set; }
+
+    public void Sail()
+    {
+        Console.WriteLine($"{Name} is a {Age}-year-old {Gender} sailor, who sails the {Ship}.");
+    }
+}
+
+internal class Pilot : Human
+{
+    public string Plane { get; set; }
+
+    public void Fly()
+    {
+        Console.WriteLine($"{Name} is a {Age}-year-old {Gender} pilot, who flies {Plane}.");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using Domain;
+
+namespace dz11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Builder bob = new Builder
+            {
+                Name = "Bob",
+                Age = 35,
+                Gender = "male",
+                Specialization = "carpenter"
+            };
+
+            Sailor alice = new Sailor
+            {
+                Name = "Alice",
+                Age = 28,
+                Gender = "female",
+                Ship = "The Black Pearl"
+            };
+
+            Pilot john = new Pilot
+            {
+                Name = "John",
+                Age = 45,
+                Gender = "male",
+                Plane = "Boeing 747"
+            };
+
+            bob.Build();
+            alice.Sail();
+            john.Fly();
+        }
+    }
+}
