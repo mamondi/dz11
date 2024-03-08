1bc4b868841495909d514c963663a0f335cd5021
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-08 10:22:47 +0200

    still not done

--- Class.cs
+++ Class.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace Domain;
+
+//Класс паспорт громадянина заданої країни - Україна
+
+public class Passport
+{
+    public string Series { get; set; }
+    public string Number { get; set; }
+    public string IssuedBy { get; set; }
+    public DateTime DateOfIssue { get; set; }
+    public DateTime DateOfExpiry { get; set; }
+    public string PersonalNumber { get; set; }
+}
+ 
