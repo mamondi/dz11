b3628eaa766fa518eb2b5f669f20313567c81a92
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-08 15:01:28 +0200

    done!!!

--- Class.cs
+++ Class.cs
@@ -1,6 +1,25 @@
 ﻿namespace Domain;
 
-//Класс паспорт громадянина заданої країни - Україна
+internal class Human
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+}
+
+internal class Builder : Human
+{
+    public string Specialization { get; set; }
+
+    public void Build()
+    {
+        Console.WriteLine($"{Name} is a {Age}-year-old {Gender} {Specialization}, who can build some cool things.");
+    }
+}
+
+internal class Sailor : Human
+{
+    public string Ship { get; set; }
 
     public void Sail()
     {
