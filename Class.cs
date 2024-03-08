1c2dd1d066920f04a001e2f9ffd14320afb5c042
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-08 15:12:05 +0200

    done

--- Class.cs
+++ Class.cs
@@ -1,14 +1,60 @@
 ﻿namespace Domain;
 
-//Класс паспорт громадянина заданої країни - Україна
+using System;
+using System.Collections.Generic;
 
 public class Passport
 {
-    public string Series { get; set; }
-    public string Number { get; set; }
-    public string IssuedBy { get; set; }
-    public DateTime DateOfIssue { get; set; }
-    public DateTime DateOfExpiry { get; set; }
-    public string PersonalNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Nationality { get; set; }
+    public string PassportNumber { get; set; }
+
+    // Конструктор класу Passport
+    public Passport(string firstName, string lastName, string nationality, string passportNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Nationality = nationality;
+        PassportNumber = passportNumber;
+    }
+
+    // Метод для виведення паспортної інформації
+    public virtual void DisplayPassportInfo()
+    {
+        Console.WriteLine($"First Name: {FirstName}");
+        Console.WriteLine($"Last Name: {LastName}");
+        Console.WriteLine($"Nationality: {Nationality}");
+        Console.WriteLine($"Passport Number: {PassportNumber}");
+    }
 }
- 
+
+public class ForeignPassport : Passport
+{
+    public List<string> VisaInformation { get; set; }
+    public string ForeignPassportNumber { get; set; }
+
+    public ForeignPassport(string firstName, string lastName, string nationality, string passportNumber, string foreignPassportNumber)
+        : base(firstName, lastName, nationality, passportNumber)
+    {
+        VisaInformation = new List<string>();
+        ForeignPassportNumber = foreignPassportNumber;
+    }
+
+    public void AddVisa(string visa)
+    {
+        VisaInformation.Add(visa);
+    }
+
+    public override void DisplayPassportInfo()
+    {
+        base.DisplayPassportInfo();
+        Console.WriteLine($"Foreign Passport Number: {ForeignPassportNumber}");
+        Console.WriteLine("Visa Information:");
+        foreach (var visa in VisaInformation)
+        {
+            Console.WriteLine(visa);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Domain;
+
+namespace dz11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passport passport = new Passport("John", "Doe", "USA", "123456");
+            passport.DisplayPassportInfo();
+            Console.WriteLine();
+
+            ForeignPassport foreignPassport = new ForeignPassport("John", "Doe", "USA", "123456", "654321");
+            foreignPassport.AddVisa("Visa 1");
+            foreignPassport.AddVisa("Visa 2");
+            foreignPassport.DisplayPassportInfo();
+        }
+    }
+}
