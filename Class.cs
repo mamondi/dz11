9cf05049b8c2e24258e41620748cb576e6c7d576
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-08 15:19:47 +0200

    done

--- Class.cs
+++ Class.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System;
+
+namespace Domain;
+
+class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Sound { get; set; }
+    public int Weight { get; set; }
+    public int Speed { get; set; }
+
+    public Animal(string name, int age, string sound, int weight, int speed)
+    {
+        Name = name;
+        Age = age;
+        Sound = sound;
+        Weight = weight;
+        Speed = speed;
+    }
+
+    public void Move()
+    {
+        Console.WriteLine($"Animal {Name} is moving with speed {Speed}");
+    }
+
+    public void MakeSound()
+    {
+        Console.WriteLine($"Animal {Name} is making sound {Sound}");
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Sound: {Sound}, Weight: {Weight}, Speed: {Speed}");
+    }
+
+}
+
+class Tiger : Animal
+{
+    public Tiger(string name, int age, string sound, int weight, int speed) : base(name, age, sound, weight, speed)
+    {
+    }
+}
+
+class Crocodile : Animal
+{
+    public Crocodile(string name, int age, string sound, int weight, int speed) : base(name, age, sound, weight, speed)
+    {
+    }
+}
+
+class Kangaroo : Animal
+{
+    public Kangaroo(string name, int age, string sound, int weight, int speed) : base(name, age, sound, weight, speed)
+    {
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using Domain;
+
+namespace dz11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger tiger = new Tiger("Tiger", 5, "Roar", 200, 60);
+            Crocodile crocodile = new Crocodile("Crocodile", 10, "Grrr", 500, 40);
+            Kangaroo kangaroo = new Kangaroo("Kangaroo", 3, "Hop", 100, 80);
+
+            tiger.ShowInfo();
+            tiger.Move();
+            tiger.MakeSound();
+
+            Console.WriteLine(" ");
+
+            crocodile.ShowInfo();
+            crocodile.Move();
+            crocodile.MakeSound();
+
+            Console.WriteLine(" ");
+
+            kangaroo.ShowInfo();
+            kangaroo.Move();
+            kangaroo.MakeSound();
+        }
+    }
+}
