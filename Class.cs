991556fe9966041e7557243269ee9e7f509f9a70
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-08 15:43:11 +0200

    done

--- Class.cs
+++ Class.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+
+namespace Domain;
+
+public abstract class Worker
+{
+    public abstract void Print();
+}
+
+public class President : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("President: The leader of the company.");
+    }
+}
+
+public class Security : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Security: Responsible for maintaining security measures.");
+    }
+}
+
+public class Manager : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Manager: Oversees a specific department or team.");
+    }
+}
+
+public class Engineer : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Engineer: Designs and builds products or systems.");
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Domain;
+
+namespace dz11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker president = new President();
+            Worker security = new Security();
+            Worker manager = new Manager();
+            Worker engineer = new Engineer();
+
+
+            Console.WriteLine("Information about workers:");
+            president.Print();
+            security.Print();
+            manager.Print();
+            engineer.Print();
+        }
+    }
+}
