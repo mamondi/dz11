45f3751de8f398cce3c60fbb758b7a9a46062d91
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-08 15:28:02 +0200

    done

--- Class.cs
+++ Class.cs
@@ -1,1 +1,62 @@
-﻿
+﻿using System;
+
+namespace Domain;
+public class Money
+{
+    public int Dollars { get; set; }
+    public int Cents { get; set; }
+
+    public Money(int dollars, int cents)
+    {
+        Dollars = dollars;
+        Cents = cents;
+    }
+
+    // Метод для виведення суми на екран
+    public void DisplayAmount()
+    {
+        Console.WriteLine($"Total amount: {Dollars}.{Cents:D2}");
+    }
+
+    // Метод для зменшення суми на вказану кількість
+    public void DecreaseAmount(int dollars, int cents)
+    {
+        if (dollars > Dollars || (dollars == Dollars && cents > Cents))
+        {
+            Console.WriteLine("Cannot decrease amount. Insufficient funds.");
+        }
+        else
+        {
+            Dollars -= dollars;
+            if (cents > Cents)
+            {
+                Dollars -= 1;
+                Cents = 100 - (cents - Cents);
+            }
+            else
+            {
+                Cents -= cents;
+            }
+            Console.WriteLine("Amount decreased successfully.");
+        }
+    }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public Money Price { get; set; }
+
+    public Product(string name, Money price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    // Метод для зменшення ціни продукту
+    public void DecreasePrice(int dollars, int cents)
+    {
+        Price.DecreaseAmount(dollars, cents);
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Domain;
+
+namespace dz11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money(10, 50);
+            money.DisplayAmount();
+            money.DecreaseAmount(5, 75);
+            money.DisplayAmount();
+
+            Product product = new Product("Bread", new Money(5, 25));
+            product.DecreasePrice(2, 50);
+            product.Price.DisplayAmount();
+        }
+    }
+}
